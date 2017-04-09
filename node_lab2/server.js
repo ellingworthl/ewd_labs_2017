a2fc2d7d1c95130c65ffbc2781a25de8dc27533a
ellingworthl <ellingworthl@hotmail.com>
2017-04-09 20:43:02 +0100

    Contacts: Week7 lab1 update. Week 8 lab2 created
    Week 8 Lab: Web APIs, ExpressWeb APIs and Express node_lab2

--- node_lab2/server.js
+++ node_lab2/server.js
@@ -1,15 +1,15 @@
 import config from './config';
 import express from 'express';
 import contactsRouter from './api/contacts';
-//CHALLENGE - correct, can mirror contacts (data: use News data from earlier lab or week 8 (first part of file)!): 
-//import postsRouter from './api/posts';
+import bodyParser from 'body-parser';//Week8 lab update (corrected typo import body-parser to import bodyParser
 
 const server = express();
 
 server.use('/api/contacts', contactsRouter);
-//CHALLENGE - correct, it is OK to put in a second server call: 
-//server.use('/api/posts', postsRouter);
 server.use(express.static('public'));
+//configure body-parser (Week 8)
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded());
 
 server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
