c73165c2c68bbdab3acde598dfc24abcabe5ed13
ellingworthl <ellingworthl@hotmail.com>
2017-04-10 19:38:38 +0100

    issues with Postman call for Contacts
    no resolution to post/put/delete requests

--- node_lab2/server.js
+++ node_lab2/server.js
@@ -1,15 +1,19 @@
 import config from './config';
 import express from 'express';
 import contactsRouter from './api/contacts';
-import bodyParser from 'body-parser';//Week8 lab update (corrected typo import body-parser to import bodyParser
+// import bodyParser from 'body-parser';//Week8 lab update (corrected typo import body-parser to import bodyParser
+import postsRouter from './api/news';//Week 8, lab2 hackerNews
 
 const server = express();
+//var server = express();
+//var bodyParser = require('body-parser')
 
 server.use('/api/contacts', contactsRouter);
 server.use(express.static('public'));
 //configure body-parser (Week 8)
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded());
+// server.use('/api/news', postsRouter);//Week 8, lab2 hackerNews
 
 server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
