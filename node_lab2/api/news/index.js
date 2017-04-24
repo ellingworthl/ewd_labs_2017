37deb98fcc1569417f0592574a4fa93b10f6dc0e
ellingworthl <ellingworthl@hotmail.com>
2017-04-24 20:15:56 +0100

    Week8/Lab2 solutions added
    get post
    add comment
    upvote comment

--- node_lab2/api/news/index.js
+++ node_lab2/api/news/index.js
@@ -14,7 +14,6 @@ router.get('/', (req, res) => {
 });
 
 
-
 //Add a post
 router.post('/', (req, res) => {
         const newPost = req.body;
@@ -44,15 +43,57 @@ router.get('/:id', (req, res) => {
 });
 
 //missed from original week8 'API' upload
+//upvote a post
 router.post('/:id/upvote', (req, res) => {
-     const id = req.params.id;
-     const result = stubAPI.upvote(id);
-	 		console.log(result)
-            if (result) {
-                 return  res.status(200).send({message: `Post ${id} Upvoted`});
-            }
+	 const id = req.params.id;
+	 const result = stubAPI.upvote(id);
+             if (result) {
+                 
+                 return  res.status(200).send({message: `Post ${id} Upvoted`}); 
+                }
             return    res.status(404).send({message: `Unable to find Post ${id}`});
+           
+});
 
+//node_lab2 solution
+//get post
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const post = stubAPI.getPost(id);
+
+       if(post){
+               return   res.status(200).send(post);
+              }
+              return    res.status(404).send({message: `Unable to find Post ${id}`}); 
+              
+            
+});
+
+//add comment
+router.post('/:id/comments', (req, res) => {
+   const id = req.params.id;
+   const comment = req.body;
+   console.log(id);
+   const result = stubAPI.addComment(id,comment.comment, comment.author);
+ if (result){
+            return  res.status(200).send({message: `Comment added to post ${id}`}); 
+      }
+            return   res.status(400).send({message: `Unable to add comment to post ${id}`});
+           
+             
+});
+
+//upvote comment
+router.post('/:postId/comments/:commentId/upvote', (req, res) => {
+   const commentId = req.params.commentId;
+   const postId = req.params.postId;
+   const result = stubAPI.upvoteComment(postId, commentId);
+             if (result) {
+                      return res.status(200).send({message: `Comment ${commentId} on post ${postId} Upvoted`}); 
+                }
+            return res.status(404).send({message: `Unable to find comment ${commentId} on post ${postId}`});
+           
+             
 });
 
 export default router;
